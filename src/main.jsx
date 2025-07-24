38a4ee8dcefbfaa29492098e6895fd708f5f74b3
abhishek61067 <abhishek.bhattarai61067@gmail.com>
2025-07-24 15:10:39 +0545

    react setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Router, RouterProvider } from "react-router";
+import { router } from "./routes/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </StrictMode>
 );
