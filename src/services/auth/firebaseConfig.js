059a2c348d227a288d31f1187db52161659fa0bd
abhishek <abhishek.bhattarai61067@gmail.com>
2025-08-23 16:32:25 +0545

    change in firebase config

--- src/services/auth/firebaseConfig.js
+++ src/services/auth/firebaseConfig.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBOsM5pqpsG6qtA8lxtu-qa_9baJ8dwQ_g",
-  authDomain: "oauth-3bac7.firebaseapp.com",
+  authDomain: "jwt-authentication-oauth-2-n4bl.onrender.com",
   projectId: "oauth-3bac7",
   appId: "1:1004038744918:web:93336e870ba20154a0c4f7",
 };
